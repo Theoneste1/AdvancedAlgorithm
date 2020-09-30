8de055ca053094a4facf996f1d9b00877178d283
Theoneste Nsanzabarinda <43376343+Theoneste1@users.noreply.github.com>
2020-09-30 18:44:42 +0200

    Update fractional_Algorithm.py
    This is just class assignment

--- fractional_Algorithm.py
+++ fractional_Algorithm.py
@@ -1,32 +1,34 @@
 import operator
 #Receiving input from the user
-n= int(input("Enter the number of objects:"))
-profit = list(map(int,input("Enter your profit values:").split()))
-weight = list(map(int,input("Enter your weight values:").split()))
-m= int(input("Enter the maximum capacity bag:"))
+numberOfObjects= int(input("Enter the number of objects:"))
+profitList = list(map(int,input("Enter your profit values:").split()))
+weightList = list(map(int,input("Enter your weight values:").split()))
+maximumNumberOfObjectsInBag= int(input("Enter the maximum capacity bag:"))
 print('Calculate the ratio(profit/weight) for each item')
 #create list and append those value and add ratio of profit and weight
 
-l=[]
-for i in range(n):
-    l.append([profit[i],weight[i],profit[i]*1.0/weight[i]])
-    
-    print(profit[i]*1.0/weight[i])
+generalList=[]
+for i in range(numberOfObjects):
+    generalList.append([profitList[i]
+    ,weightList[i],profitList[i]*1.0/weightList[i]])
+
+    print("If", weightList[i],"=>",profitList[i],"Then",profitList[i]*1.0/weightList[i])
 #sort the items based on this ratio
-l=sorted(l,reverse=True,key= operator.itemgetter(2))
+generalList=sorted(generalList,reverse=True,key= operator.itemgetter(2))
 print('Sort the item based on ratio of (profit/weight)')
-print(l)
+print(generalList)
 #take item with highest ratio and add them until we can't add the next item as whole
 max_profit=0
 fractional_object=0
-for i in range(n):
-    if m>0 and l[i][1]<m:
-        m-=l[i][1]
-        max_profit+=l[i][0]
+for i in range(numberOfObjects):
+    if maximumNumberOfObjectsInBag>0 and generalList[i][1]<maximumNumberOfObjectsInBag:
+        maximumNumberOfObjectsInBag-=generalList[i][1]
+        max_profit+=generalList[i][0]
     else:
         fractional_object=i
 #at the end add the next item as much (fraction) as we can
-if m>0:
-    max_profit+=m*(l[fractional_object][0]/(l[fractional_object][1]))
+if maximumNumberOfObjectsInBag>0:
+    max_profit+=maximumNumberOfObjectsInBag*(generalList[fractional_object][0]/
+    (generalList[fractional_object][1]))
 print("Maximum profit:")
 print(max_profit)
